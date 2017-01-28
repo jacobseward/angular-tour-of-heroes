c05b1acecf33a33eacd94d6dfb5fc6e4940f8ad0
Jacob Seward <jacobseward@Jacobs-MBP-2.home>
2017-01-28 13:54:33 -0500

    lesson 7: http (pick up w/ Search-by-name)

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,25 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      {id: 11, name: 'Mr. Nice'},
+      {id: 12, name: 'Narco'},
+      {id: 13, name: 'Bombasto'},
+      {id: 14, name: 'Celeritas'},
+      {id: 15, name: 'Magneta'},
+      {id: 16, name: 'RubberMan'},
+      {id: 17, name: 'Dynama'},
+      {id: 18, name: 'Dr IQ'},
+      {id: 19, name: 'Magma'},
+      {id: 20, name: 'Tornado'}
+    ];
+
+    let children = [
+      {id: 1, name: 'Ella'},
+      {id: 2, name: 'Milo'},
+      {id: 3, name: 'Olive'},
+    ]
+
+    return {heroes};
+  }
+}
