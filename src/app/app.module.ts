2131d252c1248e7a454a20929bde0a900abe0216
Jacob Seward <jacobseward@Jacobs-MBP-2.home>
2017-01-28 23:48:49 -0500

    minor formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
-import { HttpModule }     from '@angular/http';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -10,13 +10,13 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
-import { ChildrenComponent } from './children/children.component';
+import { ChildrenComponent }    from './children/children.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 
 import { HeroService }          from './hero.service';
-import { ChildService }          from './child.service';
+import { ChildService }         from './child.service';
 
 @NgModule({
   imports: [
